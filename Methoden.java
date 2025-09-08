9d2d24e5290c9c9b07e7d9854d7f7eed3e71aca3
Benjamin Helbling <benjamin.helbling@stud.kslzh.ch>
2025-09-08 13:53:14 +0200

    Aufgabe 1

--- Methoden.java
+++ Methoden.java
@@ -1,12 +1,20 @@
 public class Methoden {
 
     // TODO: Schreibe hier die Methoden für die Teilaufgaben.
-    
+    public static void hours(int seconds) {
+        int minutes;
+        minutes = seconds / 60;
+        seconds = seconds % 60 ;
+        int hours;
+        hours = minutes / 60 ;
+        minutes = minutes % 60 ;
+        System.out.println(((hours > 9) ? hours : "0"+ hours) + ":" + ((minutes > 9) ? minutes : "0" + minutes) + ":" +((seconds > 9) ? seconds : "0" + seconds ));
+    }
     public static void main(String[] args){
         // Test-Code für Teilaufgabe a)
         System.out.println("Aufgabe a)");
-        System.out.println(hours(3));
-        System.out.println(hours(75));
+        hours(3);
+        hours(75);
         // TODO: Schreibe mehr Testcode
         System.out.println();
 
